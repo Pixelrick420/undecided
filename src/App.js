e6e0ddcc0897238d38f9442a6ebe6bc9c5fe48f8
a0d1i2l3 <adilhaneefa2005@gmail.com>
2024-09-16 19:19:14 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        Safe, secure and quick registrations.
+        <br />
+        <span>now possible thanks to "hehe"</span>
+      </h1>
+      <p>
+        Take part in all the events you want to,
+        <br />
+        without having to <span>worry</span> about hundreds of passes
+      </p>
+      <div>
+        <button className="login-button"></button>
+        <button className="signup-button"></button>
+      </div>
     </div>
   );
 }

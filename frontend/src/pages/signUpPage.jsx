f8055bd39f58eba561f5f6749f8875ffea32feac
a0d1i2l3 <adilhaneefa2005@gmail.com>
2024-09-16 23:53:54 +0530

    font

--- frontend/src/pages/signUpPage.jsx
+++ frontend/src/pages/signUpPage.jsx
@@ -5,7 +5,7 @@ export const SignUpPage = () => {
     "What's your name?",
     "What's your email?",
     "Create a password",
-    "Already got an event? Enter the code here!"
+    "Already got an event? Enter the code here!",
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -14,18 +14,18 @@ export const SignUpPage = () => {
   const handleInputChange = (e) => setInputValue(e.target.value);
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter' && inputValue.trim() !== "") {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
       handleNextQuestion();
-    } else if (e.key === 'ArrowRight') {
+    } else if (e.key === "ArrowRight") {
       handleNextQuestion();
-    } else if (e.key === 'ArrowLeft') {
+    } else if (e.key === "ArrowLeft") {
       handlePreviousQuestion();
     }
   };
 
   const handleNextQuestion = () => {
     if (inputValue.trim() !== "" || currentQuestion === 0) {
-      setInputValue(""); 
+      setInputValue("");
       if (currentQuestion < questions.length - 1) {
         setCurrentQuestion(currentQuestion + 1);
       }
@@ -43,7 +43,7 @@ export const SignUpPage = () => {
     <div className="flex flex-col justify-center items-start h-screen text-left bg-black text-white px-6 md:px-20">
       <h1 className="ml-80 text-3xl md:text-4xl mb-4">Let's get started.</h1>
 
-      <h2 className="ml-80 text-sm md:text-base font-serif italic opacity-75 mb-6">
+      <h2 className="ml-80  md:text-base  italic opacity-75 mb-6 font-sans text-lg">
         {questions[currentQuestion]}
       </h2>
 
@@ -53,7 +53,7 @@ export const SignUpPage = () => {
         onChange={handleInputChange}
         onKeyDown={handleKeyPress}
         className="ml-80 w-full max-w-md p-3 text-white border-custom-gray rounded-md text-left input-gradient"
-        class='gradient'
+        class="gradient"
         autoFocus
       />
 

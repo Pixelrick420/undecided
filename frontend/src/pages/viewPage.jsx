d0c774438c7c3f9bc3efdb1a33591c20f2aaf856
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2024-09-25 22:17:49 +0530

    Update viewPage.jsx

--- frontend/src/pages/viewPage.jsx
+++ frontend/src/pages/viewPage.jsx
@@ -40,7 +40,7 @@ const ViewPage = () => {
   return (
     <div className="min-h-screen w-full bg-black text-white flex justify-center items-center">
       <div className="w-full max-w-xl">
-        <h2 className="text-3xl font-bold mb-4 font-serif">My Events</h2>
+        <h2 className="text-5xl font-bold mb-4 font-serif">My Events</h2>
         <p className="mb-6 font-sans text-lg">
           Here's a list of events you've registered for.
         </p>
@@ -48,7 +48,8 @@ const ViewPage = () => {
         {currentEvents.map((event, index) => (
           <div
             key={index}
-            className="flex justify-between items-center border border-gray-300 rounded-2xl p-4 mb-4 font-sans">
+            className="flex justify-between items-center border border-gray-300 rounded-2xl p-4 mb-4 font-sans"
+          >
             <span className="text-sm font-sans">{event.name}</span>
             <div className="flex items-center">
               <span className="mr-2 text-sm">{event.status}</span>
